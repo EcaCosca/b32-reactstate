82d50e496acce922e30530baebc147efa7079f45
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-20 11:40:06 +0200

    Boiler plate example for props

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = ({instructor}) => {
+  return (
+    <div>
+        <p>Welcome {instructor}, your students today are:</p>
+        <ul>
+        </ul>
+    </div>
+  )
+}
+
+export default Form
